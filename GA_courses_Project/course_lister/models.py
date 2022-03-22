06b74b878071ca04988c18bc3ecc11af7e6d95af
Kyle Neil <kyleneil1403@gmail.com>
2022-03-22 15:41:46 +0000

    Add model, database, and data

--- GA_courses_Project/course_lister/models.py
+++ GA_courses_Project/course_lister/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=128, unique=True, null=False)
+    starting_date = models.DateField(null=False)
+    description = models.CharField(max_length=255, null=False)
+
+    def __str__(self):
+        return self.title
