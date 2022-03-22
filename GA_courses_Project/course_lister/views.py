03499630f95460bff03d4853b94c111568621c80
Kyle Neil <kyleneil1403@gmail.com>
2022-03-22 16:55:20 +0000

    Add and implement template

--- GA_courses_Project/course_lister/views.py
+++ GA_courses_Project/course_lister/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from course_lister.models import Course
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("This is a test to see if this works!")
+    course_list = Course.objects.all()
+    context_dict = {}
+    context_dict['course'] = course_list
+    return render(request, 'course_lister/index.html', context=context_dict)
